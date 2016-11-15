dfdd79523958402b9034d03a8d3e70c3aed807cb
John Otander <johnotander@gmail.com>
2016-11-14 18:59:23 -0700

    Add proof of concept for typography config

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,9 +1,44 @@
 import React from 'react'
 
+import commaSplit from 'comma-split'
+
+import conf from '../config'
+const config = conf()
+
 import Layout from '../components/layout'
 
-export default () => (
-  <Layout>
-    <h1 children='Hello, world' />
-  </Layout>
-)
+export default class extends React.Component {
+  constructor () {
+    super()
+
+    this.state = { config, currTypeScale: config.typeScale.join(', ') }
+    this.handleTypeScale = this.handleTypeScale.bind(this)
+  }
+
+  handleTypeScale (e, i) {
+    const { config } = this.state
+    const typeScale = config.typeScale
+    typeScale[i] = e.target.value
+
+    const newConfig = Object.assign({}, config, { typeScale })
+    this.setState({ config: newConfig })
+  }
+
+  render () {
+    const { config, currTypeScale } = this.state
+
+    return (
+      <Layout>
+        <h3 className='f6 ttu fw6 mb0 mt5 bb pb2'>Typography</h3>
+        {config.typeScale.map((f, i) => (
+          <div>
+            <p style={{fontSize: f * 16}}>Heading {i + 1}</p>
+            <input value={config.typeScale[i]} onChange={e => this.handleTypeScale(e, i)} />
+          </div>
+        ))}
+        <h3 className='f6 ttu fw6 mb0 mt5 bb pb2'>Config</h3>
+        <pre children={JSON.stringify(config, null, 2)} />
+      </Layout>
+    )
+  }
+}
