162629a10f1ef5fdb1da28e3d9c8f67dbd7da6ff
John Otander <johnotander@gmail.com>
2016-11-15 11:19:38 -0700

    Begin working on color docs

--- app/components/layout.js
+++ app/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from './head'
 
 export default ({ config, children }) => (
-  <div>
+  <div className='pa4'>
     <Head config={config} />
     {children}
   </div>
--- app/pages/index.js
+++ app/pages/index.js
@@ -17,10 +17,19 @@ export default class extends React.Component {
 
   handleTypeScale (e, i) {
     const { config } = this.state
-    const typeScale = config.typeScale
+    const { typeScale } = config
     typeScale[i] = e.target.value
 
     const newConfig = Object.assign({}, config, { typeScale })
+    this.setState({ config: newConfig })
+  }
+
+  handleColorAdd (name, val) {
+    const { config } = this.state
+    const { colors } = config
+    colors[name] = val
+
+    const newConfig = Object.assign({}, config, { colors })
     this.setState({ config: newConfig })
   }
 
@@ -29,15 +38,35 @@ export default class extends React.Component {
 
     return (
       <Layout config={config}>
-        <h3 className='f6 ttu fw6 mb0 mt5 bb pb2'>Typography</h3>
+        <h1 className='pb4'>Tachyons Generator</h1>
+        <h3 className='f6 ttu fw6 mb0 bb pb2'>Typography</h3>
         {config.typeScale.map((f, i) => (
-          <div>
+          <div key={i}>
             <p className={`f${i + 1}`}>Heading {i + 1}</p>
             <input value={config.typeScale[i]} onChange={e => this.handleTypeScale(e, i)} />
           </div>
         ))}
+        <h3 className='f6 ttu fw6 mb0 mt5 bb pb2 mb1'>Colors</h3>
+        <table className='border-collapse mid-gray f6' cellSpacing='0' cellPadding='0'>
+          <tbody>
+            {config.colors.map(color => {
+              const name = Object.keys(color)[0]
+              const value = color[name]
+
+              return (
+                <tr key={name}>
+                  <td className={`bg-${name} pa2 bb`} style={{borderBottomColor: value}} />
+                  <td className={`${name} bb b--near-white b f4 ph2`} children='Aa' />
+                  <td className='bb b--near-white pa2'>
+                    --{name}: {value};
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
         <h3 className='f6 ttu fw6 mb0 mt5 bb pb2'>Config</h3>
-        <pre children={JSON.stringify(config, null, 2)} />
+        <pre children={JSON.stringify(config, null, 2)} className='bg-near-white pa2' />
       </Layout>
     )
   }
