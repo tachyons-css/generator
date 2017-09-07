4a1238a76d7ae0c82d31d0d7fd687efc4a549650
John Otander <johnotander@gmail.com>
2017-09-07 14:27:29 -0600

    Begin adding ability to publish generated tachyons versions

---
+++ api/index.js
@@ -0,0 +1,61 @@
+const { version } = require('./package')
+
+const crypto = require('crypto')
+const AWS = require('aws-sdk')
+const {
+  json,
+  send
+} = require('micro')
+
+AWS.config.update({
+  region: 'us-west-2',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const sts = new AWS.STS()
+
+module.exports = async (req, res) => {
+  try {
+    const config = await json(req)
+    const hash = hashConfig(config)
+    const token = await getToken(hash)
+
+    send(res, 200, { hash, token })
+  } catch (error) {
+    send(res, 500, { error })
+  }
+}
+
+const getToken = hash => {
+  return new Promise((resolve, reject) => {
+    sts.getFederationToken({
+      Name: hash,
+      DurationSeconds: 900,
+      Policy: JSON.stringify({
+        Version: '2012-10-17',
+        Statement: [{
+          Effect: 'Allow',
+          Action: ['s3:ListBucket'],
+          Resource: [`arn:aws:s3:::tachyons-pub/${version}/${hash}`]
+        }, {
+          Effect: 'Allow',
+          Action: ['s3:PutObject', 's3:PutObjectACL'],
+          Resource: [`arn:aws:s3:::tachyons-pub/${version}/${hash}/*`]
+        }]
+      })
+    }, (err, data) => {
+      err ? reject(err) : resolve(data)
+    })
+  })
+}
+
+const hashConfig = (obj, sha = '256') => {
+  const str = JSON.stringify(obj)
+
+  return crypto
+    .createHash(`sha${sha}`)
+    .update(str)
+    .digest('hex')
+    .slice(0, 32)
+}
