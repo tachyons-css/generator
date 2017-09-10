f19b724cbc97c1b8d95b7f456ed0d1e7be1b02b7
Varun Vachhar <varunvachhar@gmail.com>
2017-09-10 17:21:25 -0400

    dynamically generate docs for configurable modules

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "typeScale": [
-    3, 2.25, 1.5, 1.25, 1, 0.875
+    3, 2.25, 1.5, 1.25, 1, 0.875, 0.75
   ],
   "spacing": {
     "root": 8,
     "ratio": 2,
-    "steps": 6
+    "steps": 7
   },
   "customMedia": [
     { "m": 48 },
