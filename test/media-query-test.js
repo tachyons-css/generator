786ce4c127bed89e87bd9815c397dbe396ce4eb1
John Otander <johnotander@gmail.com>
2017-11-03 10:58:17 -0600

    Improve media query config

---
+++ test/media-query-test.js
@@ -0,0 +1,41 @@
+import test from 'ava'
+import fs from 'fs'
+import path from 'path'
+
+import config from '../config'
+import tachyonsGenerator from '../'
+
+test('handle greater than queries', async t => {
+  const tachy = tachyonsGenerator({
+    customMedia: [
+      { m: '32' },
+      {
+        l: {
+          value: '64'
+        }
+      },
+      {
+        med: {
+          value: 64,
+          minWidth: true,
+          prefix: true,
+          delimiter: '@'
+        }
+      },
+      {
+        xl: {
+          value: 123,
+          minWidth: true
+        }
+      }
+    ]
+  })
+
+  const {
+    modules: {
+      borderWidths
+    }
+  } = await tachy.generate()
+
+  t.snapshot(borderWidths)
+})
