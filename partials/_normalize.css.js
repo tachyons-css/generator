f0f242494204b7ec2a2a5bf2ec0362154054260d
Daniele Polencic <daniele.polencic@gmail.com>
2019-12-15 16:05:35 +0800

    updated normalize to 8.0.1

--- partials/_normalize.css.js
+++ partials/_normalize.css.js
@@ -1,133 +1,60 @@
 module.exports = `
-/*! normalize.css v7.0.0 | MIT License | github.com/necolas/normalize.css */
-
-
-
-
+/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
 
 html {
-  line-height: 1.15; 
-  -ms-text-size-adjust: 100%; 
-  -webkit-text-size-adjust: 100%; 
+  line-height: 1.15; /* 1 */
+  -webkit-text-size-adjust: 100%; /* 2 */
 }
-
-
-
-
 
 body {
   margin: 0;
 }
 
-
-
-article,
-aside,
-footer,
-header,
-nav,
-section {
+main {
   display: block;
 }
-
-
 
 h1 {
   font-size: 2em;
   margin: 0.67em 0;
 }
 
-
-
-
-
-figcaption,
-figure,
-main { 
-  display: block;
-}
-
-
-
-figure {
-  margin: 1em 40px;
-}
-
-
-
 hr {
-  box-sizing: content-box; 
-  height: 0; 
-  overflow: visible; 
+  box-sizing: content-box; /* 1 */
+  height: 0; /* 1 */
+  overflow: visible; /* 2 */
 }
-
-
 
 pre {
-  font-family: monospace, monospace; 
-  font-size: 1em; 
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
 }
-
-
-
-
 
 a {
-  background-color: transparent; 
-  -webkit-text-decoration-skip: objects; 
+  background-color: transparent;
 }
-
-
 
 abbr[title] {
-  border-bottom: none; 
-  text-decoration: underline; 
-  text-decoration: underline dotted; 
+  border-bottom: none; /* 1 */
+  text-decoration: underline; /* 2 */
+  text-decoration: underline dotted; /* 2 */
 }
-
-
-
-b,
-strong {
-  font-weight: inherit;
-}
-
-
 
 b,
 strong {
   font-weight: bolder;
 }
 
-
-
 code,
 kbd,
 samp {
-  font-family: monospace, monospace; 
-  font-size: 1em; 
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
 }
-
-
-
-dfn {
-  font-style: italic;
-}
-
-
-
-mark {
-  background-color: #ff0;
-  color: #000;
-}
-
-
 
 small {
   font-size: 80%;
 }
-
-
 
 sub,
 sup {
@@ -145,73 +72,37 @@ sup {
   top: -0.5em;
 }
 
-
-
-
-
-audio,
-video {
-  display: inline-block;
-}
-
-
-
-audio:not([controls]) {
-  display: none;
-  height: 0;
-}
-
-
-
 img {
   border-style: none;
 }
-
-
-
-svg:not(:root) {
-  overflow: hidden;
-}
-
-
-
-
 
 button,
 input,
 optgroup,
 select,
 textarea {
-  font-family: sans-serif; 
-  font-size: 100%; 
-  line-height: 1.15; 
-  margin: 0; 
+  font-family: inherit; /* 1 */
+  font-size: 100%; /* 1 */
+  line-height: 1.15; /* 1 */
+  margin: 0; /* 2 */
 }
 
-
-
 button,
-input { 
+input { /* 1 */
   overflow: visible;
 }
 
-
-
 button,
-select { 
+select { /* 1 */
   text-transform: none;
 }
 
-
-
 button,
-html [type="button"], 
+[type="button"],
 [type="reset"],
 [type="submit"] {
-  -webkit-appearance: button; 
+  -webkit-appearance: button;
 }
-
-
 
 button::-moz-focus-inner,
 [type="button"]::-moz-focus-inner,
@@ -221,8 +112,6 @@ button::-moz-focus-inner,
   padding: 0;
 }
 
-
-
 button:-moz-focusring,
 [type="button"]:-moz-focusring,
 [type="reset"]:-moz-focusring,
@@ -230,104 +119,63 @@ button:-moz-focusring,
   outline: 1px dotted ButtonText;
 }
 
-
-
 fieldset {
   padding: 0.35em 0.75em 0.625em;
 }
 
-
-
 legend {
-  box-sizing: border-box; 
-  color: inherit; 
-  display: table; 
-  max-width: 100%; 
-  padding: 0; 
-  white-space: normal; 
+  box-sizing: border-box; /* 1 */
+  color: inherit; /* 2 */
+  display: table; /* 1 */
+  max-width: 100%; /* 1 */
+  padding: 0; /* 3 */
+  white-space: normal; /* 1 */
 }
-
-
 
 progress {
-  display: inline-block; 
-  vertical-align: baseline; 
+  vertical-align: baseline;
 }
-
-
 
 textarea {
   overflow: auto;
 }
 
-
-
 [type="checkbox"],
 [type="radio"] {
-  box-sizing: border-box; 
-  padding: 0; 
+  box-sizing: border-box; /* 1 */
+  padding: 0; /* 2 */
 }
-
-
 
 [type="number"]::-webkit-inner-spin-button,
 [type="number"]::-webkit-outer-spin-button {
   height: auto;
 }
 
-
-
 [type="search"] {
-  -webkit-appearance: textfield; 
-  outline-offset: -2px; 
+  -webkit-appearance: textfield; /* 1 */
+  outline-offset: -2px; /* 2 */
 }
 
-
-
-[type="search"]::-webkit-search-cancel-button,
 [type="search"]::-webkit-search-decoration {
   -webkit-appearance: none;
 }
 
-
-
 ::-webkit-file-upload-button {
-  -webkit-appearance: button; 
-  font: inherit; 
+  -webkit-appearance: button; /* 1 */
+  font: inherit; /* 2 */
 }
 
-
-
-
-
-details, 
-menu {
+details {
   display: block;
 }
-
-
 
 summary {
   display: list-item;
 }
 
-
-
-
-
-canvas {
-  display: inline-block;
-}
-
-
-
 template {
   display: none;
 }
-
-
-
-
 
 [hidden] {
   display: none;
